568ce5d745942087a13694f6ffc27ee4c166df59
Maxime Regeard <regeard@apc.in2p3.fr>
2026-02-19 14:49:39 +0100

    add expectApproxEqlAbsIter for iterable data structure

---
+++ src/test.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+pub inline fn expectApproxEqAbsIter(expected: anytype, actual: anytype, tolerance: anytype) ! void {
+    inline for (0..expected.len) |i| {
+        try std.testing.expectApproxEqAbs(expected[i], actual[i], tolerance);
+    }
+}
