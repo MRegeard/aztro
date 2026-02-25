809b0a555c2866e2da993078742c18e6a0cdd68f
Maxime Regeard <regeard@apc.in2p3.fr>
2026-02-25 15:28:40 +0100

    units export

--- src/units.zig
+++ src/units.zig
@@ -12,6 +12,66 @@ pub const fraction = @import("units/fraction.zig");
 pub const Fraction = fraction.Fraction;
 pub const system = @import("units/system.zig");
 pub const System = system.System;
+pub const cgs = @import("units/cgs.zig");
+
+// EXPORT ANCHOR
+
+// si
+pub const m = si.m;
+pub const cm = si.cm;
+pub const mm = si.mm;
+pub const um = si.um;
+pub const nm = si.nm;
+pub const AA = si.AA;
+pub const ha = si.ha;
+pub const l = si.l;
+pub const rad = si.rad;
+pub const deg = si.deg;
+pub const hourangle = si.hourangle;
+pub const arcmin = si.arcmin;
+pub const arcsec = si.arcsec;
+pub const mas = si.mas;
+pub const uas = si.uas;
+pub const sr = si.sr;
+pub const s = si.s;
+pub const min = si.min;
+pub const h = si.h;
+pub const d = si.d;
+pub const sday = si.sday;
+pub const yr = si.yr;
+pub const Hz = si.Hz;
+pub const kg = si.kg;
+pub const g = si.g;
+pub const t = si.t;
+pub const mol = si.mol;
+pub const kat = si.kat;
+pub const K = si.K;
+pub const degC = si.degC;
+pub const N = si.N;
+pub const J = si.J;
+pub const Pa = si.Pa;
+pub const W = si.W;
+pub const A = si.A;
+pub const C = si.C;
+pub const V = si.V;
+pub const Ohm = si.Ohm;
+pub const S = si.S;
+pub const F = si.F;
+pub const Wb = si.Wb;
+pub const T = si.T;
+pub const H = si.H;
+pub const cd = si.cd;
+pub const lm = si.lm;
+pub const lx = si.lx;
+pub const Bq = si.Bq;
+pub const Ci = si.Ci;
+pub const Gy = si.Gy;
+pub const Sv = si.Sv;
+
+// cgs
+pub const Gal = cgs.Gal;
+
+// EXPORT ANCHOR END
 
 test {
     testing.refAllDecls(@This());
