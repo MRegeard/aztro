d7ddf961b6052181f8d9626c801d29655e8d3c75
Maxime Regeard <regeard@apc.in2p3.fr>
2026-03-03 23:44:19 +0100

    dev tools prefix table and builder

---
+++ dev/tools/prefix_builder.zig
@@ -0,0 +1,103 @@
+const std = @import("std");
+const aztro = @import("aztro");
+const units = aztro.units;
+const Unit = units.Unit;
+const si = units.si;
+const cgs = units.cgs;
+const astrophys = units.astrophys;
+const misc = units.misc;
+const imperial = units.imperial;
+const photometric = units.photometric;
+const prefix_table = @import("prefix_table.zig");
+const UnitPrefixPair = prefix_table.UnitPrefixPair;
+
+const UPP = prefix_table.UPP;
+
+const mods = .{ si, cgs, photometric, astrophys, misc, imperial };
+
+const file_path = "src/units/units_storage.zig";
+
+const imports: []const u8 =
+    \\const std = @import("std");
+    \\const Unit = @import("unit.zig").Unit;
+    \\const utils = @import("utils.zig");
+    \\const defUnitFromUnit = utils.defUnitFromUnit;
+    \\const si = @import("si.zig");
+    \\const cgs = @import("cgs.zig");
+    \\const astrophys = @import("astrophys.zig");
+    \\const misc = @import("misc.zig");
+    \\const imperial = @import("imperial.zig");
+    \\const photometric = @import("photometric.zig");
+    \\
+;
+
+const separator: []const u8 = "//////////////////////////////////////////////////////\n";
+
+const file_comment: []const u8 =
+    \\//! This module file is being generated automatically by
+    \\//! dev-tools (see dev/tools/prefix_builder).
+    \\
+;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const file = try std.fs.cwd().openFile(file_path, .{ .lock = .exclusive, .mode = .read_write });
+    defer file.close();
+
+    var buf: [128]u8 = undefined;
+    var file_writer = file.writer(&buf);
+    const writer = &file_writer.interface;
+
+    try writer.writeAll(file_comment);
+    try writer.writeByte('\n');
+
+    try writer.writeAll(imports);
+    try writer.writeByte('\n');
+    try writer.writeByte('\n');
+
+    inline for (mods) |mod| {
+        const full_module_name = @typeName(mod);
+        const dot_index = std.mem.indexOf(u8, full_module_name, ".") orelse @panic("No '.' found while triming module name");
+        const module_name = full_module_name[dot_index + 1 ..];
+        const upper_module_name = try std.ascii.allocUpperString(allocator, module_name);
+        const comments_write = try std.fmt.allocPrint(allocator, "// {s}\n", .{upper_module_name});
+        try writer.writeAll(separator);
+        try writer.writeAll(comments_write);
+        try writer.writeAll(separator);
+        try writer.writeByte('\n');
+
+        inline for (@typeInfo(mod).@"struct".decls) |decl| {
+            const prefix_comment = try std.fmt.allocPrint(allocator, "// {s}\n", .{decl.name});
+            try writer.writeAll(prefix_comment);
+            const base_unit_str = try std.fmt.allocPrint(allocator, "pub const {s} = {s}.{s};\n", .{ decl.name, module_name, decl.name });
+            try writer.writeAll(base_unit_str);
+            const unit = @field(mod, decl.name);
+
+            if (getUnitPrefixPair(unit)) |pair| {
+                for (0..pair.prefs.len) |pref_idx| {
+                    const pref = pair.prefs.list[pref_idx];
+                    const pref_str = try std.fmt.allocPrint(
+                        allocator,
+                        "pub const {s}{s}: Unit = defUnitFromUnit({s}.{s}, {e}, .initUniqueSymbol(\"{s}{s}\"));\n",
+                        .{ @tagName(pref), decl.name, module_name, decl.name, pref.value(), @tagName(pref), decl.name },
+                    );
+                    try writer.writeAll(pref_str);
+                }
+            }
+            try writer.writeByte('\n');
+        }
+    }
+
+    try writer.flush();
+    try file_writer.end();
+}
+
+pub fn getUnitPrefixPair(unit: Unit) ?UnitPrefixPair {
+    for (UPP) |pair| {
+        if (pair.unit.eqlExact(unit)) return pair;
+    }
+    return null;
+}
---
+++ dev/tools/prefix_table.zig
@@ -0,0 +1,230 @@
+const std = @import("std");
+const aztro = @import("aztro");
+const unitMod = aztro.units.unit;
+const Unit = unitMod.Unit;
+const si = aztro.units.si;
+const cgs = aztro.units.cgs;
+const ap = aztro.units.astrophys;
+const mc = aztro.units.misc;
+const pm = aztro.units.photometric;
+
+pub const ap_ptr = ALL_PREFIX;
+
+pub const UPP = [_]UnitPrefixPair{
+    // SI
+    .init(si.m, .initAllExclude(@constCast(&[_]SiPrefix{ .c, .n, .u, .m }))),
+    .init(si.AA, .init(@constCast(&[_]SiPrefix{.m}))),
+    .init(si.l, ap_ptr),
+    .init(si.rad, ap_ptr),
+    .init(si.deg, ap_ptr),
+    .init(si.arcmin, ap_ptr),
+    .init(si.arcsec, ap_ptr),
+    .init(si.sr, ap_ptr),
+    .init(si.s, ap_ptr),
+    .init(si.min, ap_ptr),
+    .init(si.h, .initAllExclude(@constCast(&[_]SiPrefix{.p}))),
+    .init(si.d, .initAllExclude(@constCast(&[_]SiPrefix{ .c, .y }))),
+    .init(si.a, .initAllExclude(@constCast(&[_]SiPrefix{ .P, .h }))),
+    .init(si.yr, ap_ptr),
+    .init(si.Hz, ap_ptr),
+    .init(si.g, .initAllExclude(@constCast(&[_]SiPrefix{.k}))),
+    .init(si.mol, ap_ptr),
+    .init(si.kat, ap_ptr),
+    .init(si.K, ap_ptr),
+    .init(si.N, ap_ptr),
+    .init(si.J, ap_ptr),
+    .init(si.Pa, ap_ptr),
+    .init(si.W, ap_ptr),
+    .init(si.A, ap_ptr),
+    .init(si.C, ap_ptr),
+    .init(si.Ohm, ap_ptr),
+    .init(si.S, ap_ptr),
+    .init(si.F, ap_ptr),
+    .init(si.Wb, ap_ptr),
+    .init(si.T, ap_ptr),
+    .init(si.H, ap_ptr),
+    .init(si.S, ap_ptr),
+    .init(si.cd, ap_ptr),
+    .init(si.lm, ap_ptr),
+    .init(si.lx, ap_ptr),
+    .init(si.Bq, ap_ptr),
+    .init(si.Ci, ap_ptr),
+    .init(si.Gy, ap_ptr),
+    .init(si.Sv, ap_ptr),
+
+    // CGS
+    .init(cgs.Gal, ap_ptr),
+    .init(cgs.erg, ap_ptr),
+    .init(cgs.dyn, ap_ptr),
+    .init(cgs.Ba, ap_ptr),
+    .init(cgs.P, ap_ptr),
+    .init(cgs.St, ap_ptr),
+    .init(cgs.k, ap_ptr),
+    .init(cgs.D, ap_ptr),
+    .init(cgs.G, ap_ptr),
+    .init(cgs.Oe, ap_ptr),
+
+    // Astrophys
+    .init(ap.au, ap_ptr),
+    .init(ap.pc, .initAllExclude(@constCast(&[_]SiPrefix{ .k }))),
+    .init(ap.lyr, ap_ptr),
+    .init(ap.ph, ap_ptr),
+    .init(ap.Jy, ap_ptr),
+    .init(ap.R, ap_ptr),
+    .init(ap.ct, .initAllExclude(@constCast(&[_]SiPrefix{ .p }))),
+
+    // misc
+    .init(mc.barn, ap_ptr),
+    .init(mc.bar, .init(@constCast(&[_]SiPrefix{ .m }))),
+    .init(mc.Torr, .init(@constCast(&[_]SiPrefix{ .m }))),
+    .init(mc.u, .initAllExclude(@constCast(&[_]SiPrefix{ .a, .da }))),
+    .init(mc.eV, ap_ptr),
+    .init(mc.Ry, ap_ptr),
+    // TODO Binary prefix for computer units
+
+    // Photometric
+    .init(pm.mgy, ap_ptr),
+
+    // Imperial
+};
+
+pub const UnitPrefixPair = struct {
+    const Self = @This();
+
+    unit: Unit,
+    prefs: PrefixList,
+
+    pub fn init(unit: Unit, prefs: PrefixList) Self {
+        return .{ .unit = unit, .prefs = prefs };
+    }
+};
+
+pub const ALL_PREFIX: PrefixList = .init(@constCast(&all_prefix_array));
+
+pub const all_prefix_array: [24]SiPrefix = .{
+    .Q,
+    .R,
+    .Y,
+    .Z,
+    .E,
+    .P,
+    .T,
+    .G,
+    .M,
+    .k,
+    .h,
+    .da,
+    .d,
+    .c,
+    .m,
+    .u,
+    .n,
+    .p,
+    .f,
+    .a,
+    .z,
+    .y,
+    .r,
+    .q,
+};
+
+pub const PrefixList = struct {
+    const Self = @This();
+
+    list: [24]SiPrefix = undefined,
+    len: usize = 0,
+
+    pub fn init(list: []SiPrefix) Self {
+        var result = Self{};
+        for (0..list.len) |i| {
+            result.list[i] = list[i];
+        }
+        result.len = list.len;
+        return result;
+    }
+
+    pub fn initAllExclude(exclude_list: []SiPrefix) Self {
+        var result = ALL_PREFIX;
+        for (exclude_list) |pref| {
+            if (result.getIdx(pref)) |idx| {
+                result.pop(idx);
+            } else @panic("Unknow prefix");
+        }
+        return result;
+    }
+
+    pub fn pop(self: *Self, idx: usize) void {
+        if (idx < self.len - 1) {
+            for (idx..self.len - 1) |i| {
+                self.list[i] = self.list[i + 1];
+            }
+            self.len -= 1;
+        }
+    }
+
+    pub fn getIdx(self: Self, pref: SiPrefix) ?usize {
+        if (self.len == 0) return null;
+        for (0..self.len) |i| {
+            if (self.list[i] == pref) return i;
+        }
+        return null;
+    }
+};
+
+pub const SiPrefix = enum {
+    const Self = @This();
+
+    Q,
+    R,
+    Y,
+    Z,
+    E,
+    P,
+    T,
+    G,
+    M,
+    k,
+    h,
+    da,
+    d,
+    c,
+    m,
+    u,
+    n,
+    p,
+    f,
+    a,
+    z,
+    y,
+    r,
+    q,
+
+    pub fn value(self: Self) f64 {
+        switch (self) {
+            .Q => return 1e30,
+            .R => return 1e27,
+            .Y => return 1e24,
+            .Z => return 1e21,
+            .E => return 1e18,
+            .P => return 1e15,
+            .T => return 1e12,
+            .G => return 1e9,
+            .M => return 1e6,
+            .k => return 1e3,
+            .h => return 1e2,
+            .da => return 10,
+            .d => return 1e-1,
+            .c => return 1e-2,
+            .m => return 1e-3,
+            .u => return 1e-6,
+            .n => return 1e-9,
+            .p => return 1e-12,
+            .f => return 1e-15,
+            .a => return 1e-18,
+            .z => return 1e-21,
+            .y => return 1e-24,
+            .r => return 1e-27,
+            .q => return 1e-30,
+        }
+    }
+};
